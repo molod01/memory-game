94fc9d4995e81f4e4e8ce974b5ca63ac82174299
molod01 <manuylovnikitos@gmail.com>
2022-06-13 21:59:20 +0300

    final

--- src/components/MemoryGame/MemoryGame.js
+++ src/components/MemoryGame/MemoryGame.js
@@ -24,8 +24,12 @@ const difficulties = {
   'Hard' : 500
 }
 
+function getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key] === value)
+}
+
 function MemoryGame() {
-  const [difficulty, setDifficulty] = useState(difficulties['Easy']);
+  const [difficulty, setDifficulty] = useState(difficulties['Medium']);
   const [cards, setCards] = useState([])
   const [disabled, setDisabled] = useState(false);
   const [firstChoice, setFirstChoice] = useState(null)
@@ -37,9 +41,44 @@ function MemoryGame() {
     setTurns(0)
   }
 
+  const changeDifficulty = (e) =>{
+    if(typeof e.target.value == 'string' && difficulty!= difficulties[e.target.value]){
+      setDifficulty(difficulties[e.target.value])
+      newGame();
+    }
+  }
+  const checkWin = () =>{
+    if(cards.length > 0 && cards.every(card => card.matched === true)){
+      alert(`won at ${getKeyByValue(difficulties, difficulty)} difficulty for ${turns} turns`.toLowerCase())
+    }
+  }
+  const resetTurn = () => {
+    setFirstChoice(null)
+    setSecondChoice(null)
+    setDisabled(false)
+  }
+  
+  const handleChoice = (card) => {
+    firstChoice ? setSecondChoice(card) : setFirstChoice(card)
+  }
+
+  const generateCards = (boardSizeX, boardSizeY) => {
+    let cards_set = []
+    for (let i = 0; i < (boardSizeX * boardSizeY) / 2; i++) {
+      let card = { img: tiles[Math.round(Math.random() * 11)] }
+      if (!cards_set.some((c) => c.img === card.img)) {
+        cards_set.push(card)
+      } else i--;
+    }
+    let cards = [...cards_set, ...cards_set]
+    return cards.sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random(), matched: false }));
+  }
+
   useEffect(()=>{
     setTimeout(() => checkWin(), 500);
   }, [cards])
+
   useEffect(() => {
     if (firstChoice && secondChoice) {
       setDisabled(true)
@@ -62,61 +101,25 @@ function MemoryGame() {
     }
   }, [firstChoice, secondChoice]);
 
-  const changeDifficult = (e) =>{
-    if(typeof e.target.value == 'string' && difficulty!= difficulties[e.target.value]){
-      setDifficulty(difficulties[e.target.value])
-      newGame();
-    }
-  }
-  const checkWin = () =>{
-    if(cards.length > 0 && cards.every(card => card.matched === true)){
-      alert(`won at ${difficulties[difficulty]} difficulty for ${turns} turns`)
-    }
-  }
-  const resetTurn = () => {
-    setFirstChoice(null)
-    setSecondChoice(null)
-    setDisabled(false)
-  }
-  const generateCards = (boardSizeX, boardSizeY) => {
-    let cards_set = []
-    for (let i = 0; i < (boardSizeX * boardSizeY) / 2; i++) {
-      let card = { img: tiles[Math.round(Math.random() * 11)] }
-      if (!cards_set.some((c) => c.img === card.img)) {
-        cards_set.push(card)
-      } else i--;
-    }
-    let cards = [...cards_set, ...cards_set]
-    return cards.sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: Math.random(), matched: false }));
-  }
 
-  const handleChoice = (card) => {
-    firstChoice ? setSecondChoice(card) : setFirstChoice(card)
-  }
   return (
     <div className='game'>
       <input type='button' value='New Game' onClick={newGame} />
       <p>Turns: {turns}</p>
-      <div className='difficult' onClick={changeDifficult}>
+      <div className='difficulty' onClick={changeDifficulty}>
         <input className={difficulty === difficulties['Easy'] ? 'active' : ''} type='button' value='Easy'/>
         <input className={difficulty === difficulties['Medium'] ? 'active' : ''} type='button' value='Medium'/>
         <input className={difficulty === difficulties['Hard'] ? 'active' : ''} type='button' value='Hard'/>
       </div>
       <div id='cards'>{
-        cards.map((card) => (
-          <Card key={card.id} card={card}
-            handleChoice={handleChoice}
-            flipped={card.matched ||
-              card === firstChoice ||
-              card === secondChoice} 
-              disabled={disabled}/>
+        cards.map(card => (
+          <Card key={card.id} card={card} handleChoice={handleChoice} 
+          flipped={ card.matched || card === firstChoice || card === secondChoice} 
+          disabled={disabled}/>
         ))}
       </div>
     </div>
   )
-
 }
-
 
 export default MemoryGame;
